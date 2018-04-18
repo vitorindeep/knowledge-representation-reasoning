c7ba8f822a388afc4681bb6adc7836340e604121
theManInSpace <v.j.r.c1997@hotmail.com>
2018-04-18 20:57:07 +0100

    Fixing some errors on "nulos interditos"
    and rearanging file

--- TP2/exercicio2.pl
+++ TP2/exercicio2.pl
@@ -48,10 +48,11 @@ excecao( filho(F,P) ) :-
 nulointerdito( xpto732 ).
 +filho( F,P ) :: (solucoes())
 
-%-----------------------------------------------------------------------------------------------------
-% Representaçao de conhecimento positivo
-%-----------------------------------------------------------------------------------------------------
 
+
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
+% Representaçao de conhecimento positivo
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 % Extensao do predicado utente: IdUt,Nome,Idade,Morada -> {V,F,D}
 utente(1, carlos, 21, famalicao).
@@ -65,7 +66,6 @@ utente(8, paulo, 16, barcelos).
 utente(9, maria, 43, barcelos).
 utente(10, luis, 51, vizela).
 
-
 % Extensao do predicado prestador: IdPrest,Nome,Especialidade,Instituicao -> {V,F,D}
 prestador(1, pres1, pediatria, csjoane).
 prestador(2, pres2, cardiologia, hospitalbraga).
@@ -76,7 +76,6 @@ prestador(6, pres6, psiquiatria, hsog).
 prestador(7, pres7, oftamologia, htrofa).
 prestador(8, pres8, reumatologia, htrofa).
 prestador(9, pres9, psiquiatria, hospitalbraga).
-
 
 % Extensao do predicado cuidado: Data,IdUt,IdPrest,Descricao,Custo -> {V,F,D}
 cuidado(01-02-2017, 1, 6, hipnose, 15).
@@ -93,17 +92,17 @@ cuidado(15-06-2017, 1, 1, terapiafala, 5).
 cuidado(18-06-2017, 2, 8, reumatomagrafia, 350).
 cuidado(18-06-2017, 2, 9, cbt, 10).
 
-
 % Extensao do predicado recibo: IdRecibo, IdUt, NomeUt, Morada, Especialidade, Instituicao, Data, Descricao, Custo -> {V,F,D}
 recibo(1, 3, marcos, anais, ginecologia, hospitalbraga, 13-02-2017, papanico, 30).
 recibo(2, 4, vitor, guimaraes, cardiologia, hospitalbraga, 20-03-2017, pacemaker, 20).
 
 
-%-----------------------------------------------------------------------------------------------------
+
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 % Representaçao de conhecimento negativo
-%-----------------------------------------------------------------------------------------------------
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+% Definições das negações fortes (não é verdadeiro ^ não é desconhecido)
 
 -utente(Id,N,I,M) :- 
 					nao(utente(Id,N,I,M)),
@@ -123,11 +122,9 @@ recibo(2, 4, vitor, guimaraes, cardiologia, hospitalbraga, 20-03-2017, pacemaker
 
 
 
-%-----------------------------------------------------------------------------------------------------
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 % Representar casos de conhecimento imperfeito incerto
-%-----------------------------------------------------------------------------------------------------
-
-
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 % ----------------------------------------------------------------------------------------------------
 % Desconhece-se a morada do utente com o id=12, correspondente à Mariana de 43 anos. 
@@ -135,18 +132,19 @@ utente(12, mariana, 43, i1).
 excecao( utente( A, B, C, D ) ) :-
 	utente( A, B, C, i1).
 
-
 % ----------------------------------------------------------------------------------------------------
-% Não se sabe qual é o nome do prestador com o id=11 do hospital de braga, nem qual a especialidade que o mesmo exerce. 
+% Não se sabe qual é o nome do prestador com o id=11 do hospital de braga,
+% nem qual a especialidade que o mesmo exerce. 
 prestador(11, i2, i3, hospitalbraga).
 excecao( prestador( A, B, C, D ) ) :-
 	prestador( A, i2, C, D).
 excecao( prestador( A, B, C, D ) ) :-
 	prestador( A, B, i3, D).
 
-
 % ----------------------------------------------------------------------------------------------------
-% Existe um recibo (idRecibo=5) do utente com id=10, intitulado luis, que vive em vizela e efetuou, no hospital de braga, hipnose na área da psiquiatria, na data 02-11-2017. Desconhece-se o custo declarado no recibo, no entanto sabe-se que se situa entre 35€ e 60€.
+% Existe um recibo (idRecibo=5) do utente com id=10, intitulado luis, que vive em vizela e efetuou,
+% no hospital de braga, hipnose na área da psiquiatria, na data 02-11-2017.
+% Desconhece-se o custo declarado no recibo, no entanto sabe-se que se situa entre 35€ e 60€.
 recibo(5, 10, luis, vizela, psiquiatria, hbraga, 02-11-2017, hipnose, i4).
 excecao( recibo(A, B, C, D, E, F, G, H, I) ) :-
 				recibo(A, B, C, D, E, F, G, H, i4).
@@ -155,74 +153,70 @@ recibo(5, 10, luis, vizela, psiquiatria, hbraga, 02-11-2017, hipnose, i4) :-
 
 
 
-%-----------------------------------------------------------------------------------------------------
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 % Representar casos de conhecimento imperfeito impreciso
-%-----------------------------------------------------------------------------------------------------
-
-
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 % ----------------------------------------------------------------------------------------------------
 % Não se sabe se a utente com id=11, com o nome Inês, residente em Lisboa, tem 23 ou 24 anos.
 excecao( utente(11,ines,23,lisboa) ).
 excecao( utente(11,ines,24,lisboa) ).
 
-
 % ----------------------------------------------------------------------------------------------------
 % Desconhece-se se o prestador com id=10, pres10, exerce dermatologia no Hospital de Braga ou no Hospital de São João.
 excecao( prestador(10, pres10, dermatologia, hospitalbraga) ).
 excecao( prestador(10, pres10, dermatologia, hsaojoao) ).
 
-
 % ----------------------------------------------------------------------------------------------------
-%Não se sabe se o cuidado "penso" prestado em 19-12-2017, pelo prestador com id=8 e com o valor monetário de 10€ corresponde ao utende com id=2 ou id=3.
+% Não se sabe se o cuidado "penso" prestado em 19-12-2017, pelo prestador com id=8 e com o 
+% valor monetário de 10€ corresponde ao utende com id=2 ou id=3.
 excecao( cuidado(19-12-2017, 2, 8, penso, 10) ).
 excecao( cuidado(19-12-2017, 3, 8, penso, 10) ).
 
-
 % ----------------------------------------------------------------------------------------------------
-% Desconhece-se se o cuidado prestado em 12-05-2017 pelo prestador com id=5 ao utente com id=8 foi quimioterapia ou radioterapia, e se o custo foi 50€ ou 70€.
+% Desconhece-se se o cuidado prestado em 12-05-2017 pelo prestador com id=5 ao utente com id=8 foi
+% quimioterapia ou radioterapia, e se o custo foi 50€ ou 70€.
 excecao( cuidado(12-05-2017, 8, 5, radioterapia, 50) ).
 excecao( cuidado(12-05-2017, 8, 5, radioterapia, 70) ).
 excecao( cuidado(12-05-2017, 8, 5, quimioterapia, 50) ).
 excecao( cuidado(12-05-2017, 8, 5, quimioterapia, 70) ).
 
-
 % ----------------------------------------------------------------------------------------------------
-% Existe um recibo (idRecibo=3) do utente com id=2, intitulado Diana, que vive na Trofa e efetuou, no hospital da trofa, uma consulta de rotina na área da nutrição, com custo de 50€. Desconhece-se, no entanto, se o recibo foi emitido em 27-03-2017 ou 28-03-2017.
+% Existe um recibo (idRecibo=3) do utente com id=2, intitulado Diana, que vive na Trofa e efetuou,
+% no hospital da trofa, uma consulta de rotina na área da nutrição, com custo de 50€.
+% Desconhece-se, no entanto, se o recibo foi emitido em 27-03-2017 ou 28-03-2017.
 excecao( recibo(3, 2, diana, trofa, nutricao, htrofa, 27-03-2017, rotina, 50) ).
 excecao( recibo(3, 2, diana, trofa, nutricao, htrofa, 28-03-2017, rotina, 50) ).
 
 
-%-----------------------------------------------------------------------------------------------------
+
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 % Representar casos de conhecimento imperfeito interdito
-%-----------------------------------------------------------------------------------------------------
-
-
+% ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 % ----------------------------------------------------------------------------------------------------
-% Não se pode saber qual a descrição do cuidado realizado a 13-08-2017, prestado pelo prestador com id=7 ao utente com id=10, sendo que o custo foi de 75€.
+% Não se pode saber qual a descrição do cuidado realizado a 13-08-2017, prestado pelo prestador com id=7
+% ao utente com id=10, sendo que o custo foi de 75€.
 cuidado( 13-08-2017, 10, 7, i5, 45).
 excecao( cuidado(A, B, C, D, E) ) :-
 	cuidado(A, B, C, i5, E).
 nulointerdito( i5 ).
-+cuidado( A, B, C, D, E ) :: ( solucoes((A,B,C,E), (cuidado( 13-08-2017, 10, 7, i5, 45), nao(nulo(i5)))), List ),
++cuidado( A, B, C, D, E ) :: ( solucoes((A,B,C,E), (cuidado( 13-08-2017, 10, 7, i5, 45), nao(nulointerdito(i5))), List),
                  		  	   comprimento( List, N ),
                  		  	   N == 0
                  		  	 ).
 
-
 % ----------------------------------------------------------------------------------------------------
-% Nunca se poderá saber, a partir do recibo com id=4, em que instituição foi prestado um peeling químico, na área da dermatologia, ao utente com id=7 e intitulado Marta, em 04-12-2017, com o custo de 30€
+% Nunca se poderá saber, a partir do recibo com id=4, em que instituição foi prestado um peeling químico,
+% na área da dermatologia, ao utente com id=7 e intitulado Marta, em 04-12-2017, com o custo de 30€.
 recibo( 4, 7, marta, guimaraes, dermatologia, i6, 04-12-2017, peelingquimico, 30 ).
 excecao( recibo(A, B, C, D, E, F, G, H, I) ) :-
 	recibo(A, B, C, D, E, i6, G, H, I).
 nulointerdito( i6 ).
-+recibo(A, B, C, D, E, F, G, H, I) :: ( solucoes((A, B, C, D, E, G, H, I), (recibo( 4, 7, marta, guimaraes, dermatologia, i6, 04-12-2017, peelingquimico, 30), nao(nulo(i6)))), List ),
++recibo(A, B, C, D, E, F, G, H, I) :: ( solucoes((A, B, C, D, E, G, H, I), (recibo( 4, 7, marta, guimaraes, dermatologia, i6, 04-12-2017, peelingquimico, 30), nao(nulointerdito(i6))), List ),
                  		  	   			comprimento( List, N ),
                  		  	  		 	N == 0
                  		  	   			).
-
-
 
 
 
@@ -230,29 +224,22 @@ nulointerdito( i6 ).
 % Manipular invariantes que designem restrições à inserção e à remoção de conhecimento do sistema
 %-----------------------------------------------------------------------------------------------------
 
-
-
 % ----------------------------------------------------------------------------------------------------
 % Invariante que năo permite a inserçăo de conhecimento de um utente com um id já existente
 % uso _ quando o dado năo é importante e năo quero saber dele
-
 +utente(Id, N, I, M) :: (solucoes(Id, utente(Id,_,_,_), S),
 						comprimento(S,L),
 						L==1).
 
-
 % ----------------------------------------------------------------------------------------------------
 % Invariante que năo permite a inserçăo de conhecimento de um prestador com um id já existente
-
 +prestador(Id, N, E, I) :: (solucoes(Id, prestador(Id,_,_,_), S),
 							comprimento(S, L),
 							L == 1).
 
-
 % ----------------------------------------------------------------------------------------------------
 % Invariante que năo permite a inserçăo de conhecimento de um cuidado quando o id do utente/prestador
 % năo existem na base de conhecimento
-
 +cuidado(Dat,U,P,D,C) :: (solucoes(P, prestador(P, _, _, _), LisP),
 						comprimento(LisP, NumP),
 						NumP == 1,
@@ -282,7 +269,6 @@ nulointerdito( i6 ).
 % ----------------------------------------------------------------------------------------------------
 % Invariante que năo permite a remocao de conhecimento de um utente năo presente na base de conhecimento
 % e com id associado a cuidado
-
 -utente(Id, N, I, M) :: (solucoes(Id, utente(Id,N,I,M), Uts),
 						comprimento(Uts, Lu),
 						Lu==1,
@@ -294,7 +280,6 @@ nulointerdito( i6 ).
 % ----------------------------------------------------------------------------------------------------
 % Invariante que năo permite a remocao de conhecimento de um prestador năo presente na base de
 % conhecimento e com o id associado a cuidado
-
 -prestador(Id, N, E, I) :: (solucoes(Id, prestador(Id,_,_,_), Prests),
 							comprimento(Prests, Lp),
 							Lp == 1,
@@ -305,7 +290,6 @@ nulointerdito( i6 ).
 
 % ----------------------------------------------------------------------------------------------------
 % Invariante que năo permite a remocao de conhecimento de um cuidado năo presente na base de conhecimento
-
 -cuidado(Dat, U, P, D, C) :: (solucoes((Dat,U,P,D,C), cuidado(Dat,U,P,D,C), Cuids),
 							comprimento(Cuids, L),
 							L == 1).
@@ -314,7 +298,6 @@ nulointerdito( i6 ).
 % ----------------------------------------------------------------------------------------------------
 % Invariante que não permite a remoção de qualquer recibo, uma vez que a informação
 % financeira nunca pode ser eliminada. Anti-fraude.
-
 -recibo(Id, U, N, M, E, I, Dat, D, C) :: fail.
 
 
@@ -323,7 +306,7 @@ nulointerdito( i6 ).
 
 
 
-% ////////////////////////////////////////// Predicados Extra /////////////////////////////////////////
+% ////////////////////////////////////////// Predicados Extra ////////////////////////////////////////
 % ----------------------------------------------------------------------------------------------------
 % Extensăo do predicado inserir: Termo -> {V,F}
 % mesmo que EVOLUCAO
